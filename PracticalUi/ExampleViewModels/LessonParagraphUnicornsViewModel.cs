5f60eb1e254cf9423e17bb1a8d929bdb27a73aac
Clara Schulz-Theißen <Schulz-Theissen@eevolution.de>
2020-11-24 16:34:50 +0100

    Started working on practical ui

---
+++ PracticalUi/ExampleViewModels/LessonParagraphUnicornsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalUi.ExampleViewModels
+{
+  class LessonParagraphUnicornsViewModel
+  {
+    public string Text { get; set; } = "I like unicorns";
+    public string Code { get; set; } = "while(true) unicorns.like();";
+    public string Result { get; set; } = "Runns forever";
+  }
+}
---
+++ PracticalUi/Views/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MahApps.Metro.Controls;
+using Prism.Regions;
+using System.Windows;
+using Unity;
+
+namespace PracticalUi.Views
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : MetroWindow
+  {
+    public MainWindow(RegionManager regionManager, IUnityContainer container)
+    {
+      InitializeComponent();
+
+      //regionManager.RegisterViewWithRegion("MainRegion", () => container.Resolve<LessonsOverviewView>());
+      regionManager.RegisterViewWithRegion("MainRegion", () => container.Resolve<LessonView>());
+    }
+  }
+}
